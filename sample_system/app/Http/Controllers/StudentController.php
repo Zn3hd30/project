ba0748d057c29dc8dadd3f2f83330f75e451c0db
redgutzdev3 <redgutzdev3@gmail.com>
2023-02-09 09:27:39 +0800

    initial commit

---
+++ sample_system/app/Http/Controllers/StudentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    //
+    public function show()
+    {
+        $students = Student::all();
+        // print_r(json_encode($students));
+        // die();
+        return view('Student', compact('students'));
+    }
+
+    public function store()
+    {
+        $students =    [
+            'FirstName' => 'joefel',
+            'LastName' => 'flores',
+            'MiddleName' => 'jamindang',
+            'Email' => 'jflores@ssct.edu.ph',
+            'Gender' => 'Male',
+            'Mobile' => '1',
+            'Address' => 'san juan',
+        ];
+        $update = Student::where('StudentID','=','3')->update($students);
+        if($update){
+            echo 'success';
+        } else {
+            echo 'error';
+        }
+    }
+}
